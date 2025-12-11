b7acf5a2744921ef59aefbc737c100d3188d09a1
Sour3 <turtlewong007@gmail.com>
2025-12-11 02:52:30 -0800

    Added 1.20.6 for NeoForge

---
+++ neoforge/ng-7.0/1.20.6/src/main/java/com/therainbowville/minegasm/common/Minegasm.java
@@ -0,0 +1,24 @@
+package com.therainbowville.minegasm.common;
+
+import com.therainbowville.minegasm.config.ConfigHolder;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.config.ModConfig;
+import net.neoforged.neoforge.common.NeoForge;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(Minegasm.MOD_ID)
+public class Minegasm {
+    public static final String MOD_ID = "minegasm";
+    public static final String NAME = "Minegasm";
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public Minegasm(IEventBus modEventBus, ModContainer container) {
+        container.registerConfig(ModConfig.Type.CLIENT, ConfigHolder.CLIENT_SPEC);
+        container.registerConfig(ModConfig.Type.SERVER, ConfigHolder.SERVER_SPEC);
+        
+//        NeoForge.EVENT_BUS.register(this);
+    }
+}
