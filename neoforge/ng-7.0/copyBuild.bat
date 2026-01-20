fca61f5f8b8a34cd56c45c7260a817bc079810c8
Sour3 <31393967+Sour-o7@users.noreply.github.com>
2026-01-19 22:21:56 -0800

    Delete neoforge/ng-7.0/copyBuild.bat

--- neoforge/ng-7.0/copyBuild.bat
+++
@@ -1,3 +0,0 @@
-copy "C:\Users\turtl\Documents\GitHub\minegasm\neoforge\ng-7.0\1.21.dev\build\libs\minegasm-0.6.0a-1.21-Neo-21.0.167.jar" "C:\Users\turtl\AppData\Roaming\.minecraft\mods"
-
-copy "C:\Users\turtl\Documents\GitHub\minegasm\neoforge\ng-7.0\1.21.dev\build\libs\minegasm-0.6.0a-1.21-Neo-21.0.167.jar" "C:\Users\turtl\Documents\Mod Development\Test Server\mods"
